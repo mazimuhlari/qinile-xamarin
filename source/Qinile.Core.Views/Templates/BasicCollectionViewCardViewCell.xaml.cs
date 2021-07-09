41c82be340801bd7cb3501864cd4e259d61356f2
MaziMuhlari <mazi.muhlari@gmail.com>
2021-07-09 09:42:04 +0200

    Added the vies project with a sample template

---
+++ source/Qinile.Core.Views/Templates/BasicCollectionViewCardViewCell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Qinile.Core.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BasicCollectionViewCardViewCell : ViewCell
+    {
+        public BasicCollectionViewCardViewCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
